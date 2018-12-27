7f056bcfe656b2042ad8426d040a5e59f6e1bcab
Josh Choo <dev@joshchoo.com>
2018-12-27 13:57:49 +0700

    Bringup for OP5 Pie kernel
    Signed-off-by: Josh Choo <dev@joshchoo.com>

---
+++ ramdisk/init.renderzenith.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+# Schedutil config
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
