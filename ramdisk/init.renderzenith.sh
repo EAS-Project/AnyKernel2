71e91d3aadfb1ff6687af2587190cad23c3e2965
Josh Choo <dev@joshuous.com>
2019-01-12 14:23:34 +0800

    init: Set schedtune and cpuset config

--- ramdisk/init.renderzenith.sh
+++ ramdisk/init.renderzenith.sh
@@ -3,3 +3,11 @@
 # Schedutil config
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+
+# SchedTune
+echo "1" > /dev/stune/foreground/schedtune.prefer_idle
+echo "1" > /dev/stune/top-app/schedtune.prefer_idle
+echo "1" > /dev/stune/top-app/schedtune.boost
+
+# cpuset
+echo "0-3" > /dev/cpuset/restricted/cpus
