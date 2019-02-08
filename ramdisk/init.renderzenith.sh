f6f5e239fde93a3e4c62e069b00424fab1e06443
Josh Choo <dev@joshuous.com>
2019-02-08 21:52:18 +0800

    init: Update top-app schedtune values and add foreground cpuset tuning
    Reduce power consumption for non-interactive use cases, such as
    listening to music and watching videos.
    
    Prevent foreground tasks from contending with top-app tasks on CPUs 4
    and 5.

--- ramdisk/init.renderzenith.sh
+++ ramdisk/init.renderzenith.sh
@@ -7,7 +7,9 @@ echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 # SchedTune
 echo "1" > /dev/stune/foreground/schedtune.prefer_idle
 echo "1" > /dev/stune/top-app/schedtune.prefer_idle
-echo "1" > /dev/stune/top-app/schedtune.boost
+echo "0" > /dev/stune/top-app/schedtune.boost
+echo "1" > /dev/stune/top-app/schedtune.sched_boost
 
 # cpuset
+echo "0-3,6-7" > /dev/cpuset/foreground/cpus
 echo "0-3" > /dev/cpuset/restricted/cpus
